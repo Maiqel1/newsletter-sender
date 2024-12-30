8990493d241d300e7bf2d8d0fe201f078ebd436c
maiqel <michaelolowe321@gmail.com>
2024-12-30 19:14:43 +0100

    feat : disabled buttton if content is emptu

--- src/app/page.js
+++ src/app/page.js
@@ -235,8 +235,12 @@ export default function Home() {
               <div className='space-x-4'>
                 <button
                   type='submit'
-                  disabled={isLoading}
-                  className='px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50'
+                  disabled={isLoading || !content}
+                  className={
+                    isLoading || !content
+                      ? `px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50 cursor-not-allowed`
+                      : `px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50`
+                  }
                 >
                   {isLoading ? "Sending..." : "Send Newsletter"}
                 </button>
